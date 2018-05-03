d6d438ffe2fbf8e5dbced57e99143d1f2b44a67d
cwysocki92 <38924907+cwysocki92@users.noreply.github.com>
2018-05-03 15:35:07 -0400

    create react app
    Initial commit of create react app and a few other files including the basis of the input form

---
+++ countires_ui/src/components/InputForm.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import '../styles/InputForm.css';
+
+class InputForm extends Component {
+  render() {
+    return (
+      <div>
+      test text, delete this later, using this as a reference
+      </div>
+    );
+  }
+}
+
+export default InputForm;
