b05cfee00e126eeee5acdb9387f30ce2ba2e97ab
cwysocki92 <38924907+cwysocki92@users.noreply.github.com>
2018-06-18 15:31:53 -0400

    Removing checks from PR feedback
    submissionError isn't necessary

--- countries_ui/src/components/InputForm.component.js
+++ countries_ui/src/components/InputForm.component.js
@@ -129,7 +129,7 @@ class InputForm extends Component {
 					{submissionError && 
 						<span className="submission-error">Country Name or a 2 or 3 character Country Code is required</span>
 					}
-					{submitted && !results && !submissionError && !networkError &&
+					{submitted && !results && !networkError &&
 						<span className="submission-error">No Results found</span>
 					}
 					{networkError &&
