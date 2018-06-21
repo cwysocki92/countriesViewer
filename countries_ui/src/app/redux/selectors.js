8fb6842875b756eca873ead533224ac5b6ffa30a
cwysocki92 <38924907+cwysocki92@users.noreply.github.com>
2018-06-21 19:08:35 -0400

    Fixing incorrect selector value for getCountryCode

--- countries_ui/src/app/redux/selectors.js
+++ countries_ui/src/app/redux/selectors.js
@@ -1,5 +1,5 @@
 export const getCountryResults = state => state.countriesViewer.countries;
 export const getCountryName = state => state.countriesViewer.countryName;
-export const getCountryCode = state => state.countriesViewer.getCountryCode;
+export const getCountryCode = state => state.countriesViewer.countryCode;
 export const useFullName = state => state.countriesViewer.fullname;
 export const getNetWorkError = state => state.countriesViewer.networkError;
